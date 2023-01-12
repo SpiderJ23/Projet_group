17208c85c082abf595fd4e85217ef40045c33e94
Calvin <Calvin-j@hotmail.fr>
2023-01-12 13:28:26 +0100

    models

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ app.use(bodyParser.urlencoded({ extended : false }));
 require ("dotenv").config()
 
 
+const User = require('./models/User');
+
+
 var dbURL = process.env.DATABASE_URL
 
 mongoose.set('strictQuery', false)
@@ -34,23 +37,62 @@ app.get('/HomePage', function (req, res) {
 app.get('/HomeAdmin', function (req, res) {
     res.render('Admin');
 });
-app.get('/Login', function (req, res) {
-    res.render('UserPage');
-});
+
 app.get('/Avis', function (req, res) {
     res.render('Avis');
 });
-app.get('/Sign', function (req, res) {
-    res.render('Inscription');
-});
+
 // app.get('/Avis', function (req, res) {
 //     res.render('Avis');
 // });
 
 
 
+// inscription
 
+app.post("/api/signup", function(req, res){
+    const Data = new User ({
+        user: req.body.user,
+        email: req.body.email,
+        password:bcrypt.hashSync(req.body.password,10),
+        admin: false 
+    })
+    Data.save().then(() => {
+        res.redirect("/Login");
+    }).catch(err => console.log(err));
+})
 
-var server = app.listen(5000, function () {
-    console.log("Le Serveur est en route 5000");
+app.get('/Sign', function (req, res) {
+    res.render('Inscription');
 });
+
+
+
+
+// connexion
+
+app.post("/api/signin", function(req, res){
+    User.findOne({ 
+     email : req.body.email
+ }).then(user => {
+     if(!user){
+         res.status(404).send("email Invalid !");
+     }
+     
+     if (!bcrypt.compareSync(req.body.password,user.password)){
+         res.status(404).send("Password Invalid !");
+     }
+         res.render("Home",{data:user});
+       
+     }).catch(err=>{console.log(err)})
+    });
+
+    app.get('/Login', function (req, res) {
+        res.render('UserPage');
+    });
+
+
+
+    var server = app.listen(5000, function () {
+        console.log("Le Serveur est en route 5000");
+    });
